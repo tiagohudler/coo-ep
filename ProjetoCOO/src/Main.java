d59e3a305a1c46405729df3d3a4c7ce4b1c4fda2
Tiago Hudler <tiagohudler@gmail.com>
2024-05-17 16:32:51 -0300

    intial player object

--- ProjetoCOO/src/Main.java
+++ ProjetoCOO/src/Main.java
@@ -53,6 +53,18 @@ public class Main {
 		
 		return freeArray;
 	}
+
+	class player {
+		int player_state = ACTIVE;								// estado
+		double player_X = GameLib.WIDTH / 2;					// coordenada x
+		double player_Y = GameLib.HEIGHT * 0.90;				// coordenada y
+		double player_VX = 0.25;								// velocidade no eixo x
+		double player_VY = 0.25;								// velocidade no eixo y
+		final double player_radius = 12.0;						// raio (tamanho aproximado do player)
+		double player_explosion_start = 0;						// instante do início da explosão
+		double player_explosion_end = 0;						// instante do final da explosão
+		long player_nextShot;									// instante a partir do qual pode haver um próximo tiro
+	}
 	
 	/* Método principal */
 	
