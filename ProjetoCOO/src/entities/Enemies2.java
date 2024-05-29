afc00e8cdb38e7d601733098230412127b2d610a
Tiago Hudler <tiagohudler@gmail.com>
2024-05-28 23:38:35 -0300

    fix enemy 2 spawn when exploding

--- ProjetoCOO/src/entities/Enemies2.java
+++ ProjetoCOO/src/entities/Enemies2.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class Enemies2 {
     private States states = new States();
     private final double radius = 12.0;
+    private int spawned = 0;
     private long nextEnemy = System.currentTimeMillis() + 2000;
-    double spawnX = GameLib.WIDTH * 0.20;
+    private double spawnX = GameLib.WIDTH * 0.20;
     private List<Enemy2> enemies = new ArrayList<Enemy2>();
 
     public int nEnemies (){
@@ -51,13 +52,15 @@ public class Enemies2 {
 
     public void spawnEnemy() {
         if (System.currentTimeMillis() > this.nextEnemy){
-            if (this.nEnemies() < 10){
+            if (spawned < 10){
                 enemies.add(new Enemy2(this.spawnX));
                 this.nextEnemy = System.currentTimeMillis()+120;
+                spawned++;
             }
             else{
                 this.spawnX = Math.random() > 0.5 ? GameLib.WIDTH * 0.2 : GameLib.WIDTH * 0.8;
                 this.nextEnemy = (long) (System. currentTimeMillis() + 3000 + Math.random() * 3000);
+                this.spawned = 0;
             }
         }
     }
